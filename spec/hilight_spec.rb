45773ef16dfcaa1c71e32136966a0f9c65165f51
Ernie Brodeur <ebrodeur@ujami.net>
2019-03-03 18:04:25 -0800

    more green.

--- spec/hilight_spec.rb
+++ spec/hilight_spec.rb
@@ -1,10 +1,64 @@
+require 'spec_helper'
+
+Hilight::Pattern = Struct.new :regexp, :replacement
+Hilight::Pattern.define_method(:output) do |input|
+  # map our colors in to the matches
+  regexp.names.map { |n| replacement.gsub!("\\k<#{n}>") { |s| Term::ANSIColor.color(n, s) } }
+  # map our input into the output, return the original if it doesn't map (replace) anything.
+  input.gsub!(regexp, replacement) || input
+end
+
+Hilight::Patterns = Struct.new :patterns
+Hilight::Patterns.define_method(:output) do |input|
+  patterns.each { |pattern| input = pattern.output(input) }
+  input
+end
+
 RSpec.describe Hilight do
+  let(:regexp) { /(?<green>'.*')|(?<blue>".*")/ }
+  let(:replacement) { '\k<green>\k<blue>' }
+  let(:string) { "here is 'an inline comment' and \"another\"" }
+
   it "has a version number" do
     expect(Hilight::VERSION).not_to be nil
   end
 
-  it "is expected to highlight the output of 'rspec'"
-  it "is expected to execute everything on ARGV"
-  it "is expected to highlight the output"
-  it "is expected to lookup highlight template"
+  describe Hilight::Patterns do
+    it { is_expected.to have_attributes(patterns: a_kind_of(Array).or(be_nil)) }
+
+    describe "#output" do
+      let(:pattern) { Hilight::Pattern[regexp, replacement] }
+      let(:subject) { described_class[[pattern]].output(string) }
+
+      it "is expected to run each pattern over the input string" do
+        expect(subject).to include(Term::ANSIColor.blue).and(include(Term::ANSIColor.green))
+      end
+
+      it { is_expected.to be_a_kind_of String }
+    end
+  end
+
+  describe Hilight::Pattern do
+    it { is_expected.to have_attributes(regexp: a_kind_of(Regexp).or(be_nil)) }
+    it { is_expected.to have_attributes(replacement: a_kind_of(String).or(be_nil)) }
+    it { is_expected.to respond_to(:output).with(1).arguments }
+
+    describe "#output" do
+      let(:subject) { described_class[regexp, replacement].output(string) }
+
+      it { is_expected.to be_a_kind_of String }
+
+      it "is expected to include ansi color codes" do
+        expect(subject).to include(Term::ANSIColor.blue)
+      end
+
+      context "when no replacement is found" do
+        let(:string) { 'not a matching string'}
+
+        it "is expected to return the original string" do
+          expect(subject).to eq string
+        end
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "hilight"
+require 'term/ansicolor'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
