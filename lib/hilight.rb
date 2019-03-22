a178ccafeaa3912912a9fe2cab44c90bc5295dd1
Ernie Brodeur <ebrodeur@ujami.net>
2019-03-21 22:09:22 -0700

    .

--- lib/hilight.rb
+++ lib/hilight.rb
@@ -29,37 +29,7 @@ Hilight::Pattern.define_method(:transform) do |input|
   output
 end
 
-Hilight::Pattern.define_method(:offsets) do |input|
-  offsets = {}
-  offsets['clear'] = []
-
-  m = regexp.match input
-  while m
-    m.named_captures.each do |color, string|
-      next unless string
-
-      offsets[color] ||= []
-      offsets[color].push m.offset(color)[0]
-      offsets['clear'].push m.offset(color)[1]
-    end
-
-    m = regexp.match(m.post_match)
-  end
-  offsets
-end
-
-# Hilight::Pattern.define_method(:recursive_replace) do |input, output = StringIO.new|
-#   match = regexp.match input
-#   return input unless match
-
-#   output << match.pre_match
-#   color, sub_string = match.named_captures.find { |_key, value| !value.nil? }
-#   output << Term::ANSIColor.color(color, sub_string)
-#   recursive_replace(match.post_match, output) unless match.post_match.blank?
-#   output
-# end
-
 Hilight::Fabric = Struct.new :collection
-Hilight::Fabric.define_method(:transform) do |input, stop_on_first_match: false|
+Hilight::Fabric.define_method(:transform) do |input|
   Hilight::Pattern[Regexp.union collection.map(&:regexp)].transform input
 end
